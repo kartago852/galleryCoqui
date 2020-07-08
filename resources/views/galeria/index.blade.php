2d0332d0daffcaa56b1e5e3c3592ea1afd8f795c
Franco Pacori Aliaga <franco_pacori@hotmail.com>
2020-07-08 13:15:24 -0500

    F.P[GALLERYCOQUI]-ConfiguracionFondosPages

--- resources/views/galeria/index.blade.php
+++ resources/views/galeria/index.blade.php
@@ -5,7 +5,7 @@
 
 <!-- Title Page -->
 <section class="bg-title-page flex-c-m p-t-160 p-b-80 p-l-15 p-r-15"
-    style="background-image: url(images/bg-title-page-02.jpg);">
+    style="background-image: url(images/gallery-home.jpg);">
     <h2 class="tit6 t-center">
         Gallery
     </h2>
